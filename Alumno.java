617c5291b13daa681507638496e171a47782ee60
steven <stevenvallejo99@gmail.com>
2023-05-23 17:48:03 +0200

    Creamos el constructor

--- Alumno.java
+++ Alumno.java
@@ -1,4 +1,11 @@
 
 public class Alumno {
 	private String nombre,apellido,dni;
+	
+	Alumno (String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	
 }
